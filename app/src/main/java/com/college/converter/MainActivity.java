db3492543b58db033c6456996b09720aaf84f7e5
TheaYe08 <ye000056@algonquinlive.com>
2024-02-23 18:06:29 -0500

    created log.i in convertCurrency()

--- app/src/main/java/com/college/converter/MainActivity.java
+++ app/src/main/java/com/college/converter/MainActivity.java
@@ -27,7 +27,6 @@ public class MainActivity extends AppCompatActivity {
     static private final Float CONVERSION_RATE = 0.80F;
     static final String TAG = "MainActivity";
 
-    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,10 +53,7 @@ public class MainActivity extends AppCompatActivity {
 
             Float resultFloat = inputAmountDecimal * CONVERSION_RATE;
 
-            //resultView.setText( resultFloat + " Euros" );
-            String resultString = getString(R.string.result_euros, resultFloat);
-            resultView.setText(resultString);
-
+            resultView.setText( resultFloat + " Euros" );
         }
         Log.i(TAG,"Enter convertCurrency()");
 
